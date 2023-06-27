c0b4304d2a15d2b2762c5ae8306a22cc6d9f79c1
Anna Cai <annacai@meta.com>
2023-06-27 23:15:36 +0000

    copy FSDP HF T5 code from pytorch examples

---
+++ distributed_shampoo/experiments/model_checkpointing/__init__.py
@@ -0,0 +1,10 @@
+from .checkpoint_handler import (
+    load_distributed_model_checkpoint,
+    load_model_checkpoint,
+    load_model_sharded,
+    load_optimizer_checkpoint,
+    save_distributed_model_checkpoint,
+    save_model_and_optimizer_sharded,
+    save_model_checkpoint,
+    save_optimizer_checkpoint,
+)
---
+++ distributed_shampoo/experiments/utils/__init__.py
@@ -0,0 +1,10 @@
+from .environment import bfloat_support
+from .train_utils import (
+    cleanup,
+    format_metrics_to_gb,
+    get_date_of_run,
+    setup,
+    setup_model,
+    train,
+    validation,
+)
